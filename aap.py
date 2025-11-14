3e9301d3e10fbe5dbb577861b6a7598382b6546a
rafa5115 <59081061+rafa5115@users.noreply.github.com>
2025-11-13 21:31:11 -0300

    Update aap.py

--- aap.py
+++ aap.py
@@ -18,7 +18,8 @@ def download_youtube_video(url):
             "ffmpeg_location": FFMPEG_PATH,    # ffmpeg oficial do Linux
             "quiet": True,
             "noprogress": True,
-            "cookiefile": "cookies.txt"
+            "cookiefile": "cookies.txt",
+            "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
